ad697964331d753c245068f4723ca966c1e3ed7b
Maliha Aziz <aziz.maliha@gmail.com>
2026-04-14 14:54:34 -0400

    Update cgmlstFinder_nonHPC.sh

--- Food-epidemiology/host_element_v2/pipeline_modules/cgmlstFinder/cgmlstFinder_nonHPC.sh
+++ Food-epidemiology/host_element_v2/pipeline_modules/cgmlstFinder/cgmlstFinder_nonHPC.sh
@@ -9,10 +9,10 @@
 
 STARTTIMER="$(date +%s)"
 # Script and Tools Paths
-CGE_DB_Path="/GWSPH/groups/liu_price_lab/pegasus_bin/Tools/CGE_tools/cgmlstfinder_db" # Obtained from https://bitbucket.org/genomicepidemiology/cgmlstfinder_db/src/master/
-CGE_KMA_Tool_Path="/GWSPH/groups/liu_price_lab/pegasus_bin/Tools/CGE_tools/kma/kma"
-CGE_Tool_Path="/scratch/liu_price_lab/ehsung/github/paper_shared_gits/general/Food-epidemiology/host_element_v2/pipeline_modules/cgmlstFinder/cgMLSTFinder_git/" # Git clone repo under pipeline_modules/cgmlstFinder
-cgmlstfinder_scripts="/scratch/liu_price_lab/ehsung/github/paper_shared_gits/general/Food-epidemiology/host_element_v2/pipeline_modules/cgmlstFinder"
+CGE_DB_Path="/PATH TO CGMLST DIR/cgmlstfinder_db" # Obtained from https://bitbucket.org/genomicepidemiology/cgmlstfinder_db/src/master/
+CGE_KMA_Tool_Path="/PATH TO KMA DIR/kma"
+CGE_Tool_Path="/github/Food-epidemiology/host_element_v2/pipeline_modules/cgmlstFinder/cgMLSTFinder_git" # Git clone repo under pipeline_modules/cgmlstFinder
+cgmlstfinder_scripts="/github/Food-epidemiology/host_element_v2/pipeline_modules/cgmlstFinder"
 
 # User Inputs
 Data_Folder_input=$1
@@ -34,6 +34,7 @@ for filepath in "$Data_Folder_input"/*.*; do
     fileInput=$(basename "$filepath")
     filename="${fileInput%%.*}"
     echo "$filename"
+    mkdir $filename
     # Run cgmlstfinder
     echo "Performing cgmlstfinder on: $fileInput"
     $CGE_Tool_Path/cgMLST_EHS_Modified.py -i ${Data_Folder_input}/$fileInput -s ecoli -db $CGE_DB_Path -k $CGE_KMA_Tool_Path -o $filename
@@ -44,4 +45,3 @@ for filepath in "$Data_Folder_input"/*.*; do
     echo "Extracting cgmlst md5 results into kmodes_ready_inputfile: $line"
     cat $filename/ecoli_results_md5_conversion.txt | tail -n +2  >> kmodes_ready_inputfile.txt
 done
- 
